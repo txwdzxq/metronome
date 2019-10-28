c1854a4e4e3a780c908493b33ebdc38d6e6cc428
txwdzxq <txwdzxq@sina.com>
2019-10-28 11:49:45 +0800

    2019年10月28日 11:49:42

--- src/main/java/com/mi/stream/guitar/web/controller/index/IndexController.java
+++ src/main/java/com/mi/stream/guitar/web/controller/index/IndexController.java
@@ -19,6 +19,7 @@ public class IndexController extends BaseController {
     @GetMapping(value = "hello")
     @ResponseBody
     public String hello() {
+        
         return "hello";
     }
     
